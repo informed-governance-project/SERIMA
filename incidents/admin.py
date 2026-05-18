38c64ac0b1904c52ca4aba6447cf4c897d16b624
Juan Rocha <juan.rocha@nc3.lu>
2026-05-18 11:15:27 +0200

    [NI][Admin] Added action to toggle active status of selected SectorRegulation items, comment #767

--- incidents/admin.py
+++ incidents/admin.py
@@ -261,7 +261,7 @@ class PredefinedAnswerInline(CustomTranslatableTabularInline):
     extra = 0
 
 
-@admin.action(description="Duplicate selected items")
+@admin.action(description=_("Duplicate selected items"))
 def duplicate_objects(modeladmin, request, queryset):
     config_by_model = {
         "Question": {
@@ -795,6 +795,20 @@ class SectorRegulationInline(admin.TabularInline):
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
+@admin.action(description=_("Toggle active status of selected items"))
+def toggle_active_status(modeladmin, request, queryset):
+    for obj in queryset:
+        user = request.user
+        if user.regulators.exists() and obj.regulator not in user.regulators.all():
+            messages.warning(
+                request,
+                _("You don't have permission to change the status of %s.") % obj,
+            )
+            continue
+        obj.active = not obj.active
+        obj.save()
+
+
 @admin.register(SectorRegulation, site=admin_site)
 class SectorRegulationAdmin(PermissionMixin, CustomTranslatableAdmin):
     list_display = ["active", "name", "regulation", "regulator", "is_detection_date_needed"]
@@ -804,6 +818,7 @@ class SectorRegulationAdmin(PermissionMixin, CustomTranslatableAdmin):
         "regulator__translations__name",
         "regulation__translations__label",
     ]
+    actions = [toggle_active_status]
     inlines = (SectorRegulationInline,)
     save_as = True
     filter_horizontal = ("sectors",)
