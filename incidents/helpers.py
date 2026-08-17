c45b6cd4885b5c1998c0fe35027d77107ffc8779
Juan Rocha <juan.rocha@nc3.lu>
2026-08-17 10:27:19 +0200

    [NI] Fixed calculation of deadline exceeded by using the correct detection date, comment #853

--- incidents/helpers.py
+++ incidents/helpers.py
@@ -30,7 +30,7 @@ def is_deadline_exceeded(report, incident):
                 if last_report is not None:
                     detection_date = last_report.report_timeline.incident_detection_date
             if detection_date is not None:
-                dt = actual_time - incident.incident_detection_date
+                dt = actual_time - detection_date
                 if math.floor(dt.total_seconds() / 60 / 60) >= sr_workflow.delay_in_hours_before_deadline:
                     return "OUT"
         elif sr_workflow.trigger_event_before_deadline == "NOTIF_DATE":
