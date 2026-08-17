12b4e0f20b25d1983a862ced2d92c9d047d41b92
Juan Rocha <juan.rocha@nc3.lu>
2026-08-17 10:47:51 +0200

    [NI] Refactor deadline check logic in is_deadline_exceeded function for clarity and correctness, comment #853

--- incidents/helpers.py
+++ incidents/helpers.py
@@ -37,15 +37,19 @@ def is_deadline_exceeded(report, incident):
             dt = actual_time - incident.incident_notification_date
             if math.floor(dt.total_seconds() / 60 / 60) >= sr_workflow.delay_in_hours_before_deadline:
                 return "OUT"
-        elif sr_workflow.trigger_event_before_deadline == "PREV_WORK" and incident.get_previous_workflow(report) is not False:
+        elif sr_workflow.trigger_event_before_deadline == "PREV_WORK":
             previous_workflow = incident.get_previous_workflow(report)
-            previous_incident_workflow = (
-                IncidentWorkflow.objects.all().filter(incident=incident, workflow=previous_workflow.workflow).order_by("-timestamp").first()
-            )
-            if previous_incident_workflow is not None:
-                dt = actual_time - previous_incident_workflow.timestamp
-                if math.floor(dt.total_seconds() / 60 / 60) >= sr_workflow.delay_in_hours_before_deadline:
-                    return "OUT"
+            if previous_workflow is not False:
+                previous_incident_workflow = (
+                    IncidentWorkflow.objects.all()
+                    .filter(incident=incident, workflow=previous_workflow.workflow)
+                    .order_by("-timestamp")
+                    .first()
+                )
+                if previous_incident_workflow is not None:
+                    dt = actual_time - previous_incident_workflow.timestamp
+                    if math.floor(dt.total_seconds() / 60 / 60) >= sr_workflow.delay_in_hours_before_deadline:
+                        return "OUT"
 
     return "UNDE"
 
