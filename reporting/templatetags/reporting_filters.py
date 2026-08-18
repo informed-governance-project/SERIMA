3ca6a9bd7bf1b073a0b6f1f52d8386302139392d
Juan Rocha <juan.rocha@nc3.lu>
2026-08-18 15:12:15 +0200

    [RG] Removed unused security objective and risk analysis template tags, comment #853

--- reporting/templatetags/reporting_filters.py
+++ reporting/templatetags/reporting_filters.py
@@ -5,20 +5,5 @@ register = template.Library()
 
 
 @register.simple_tag
-def security_objective_exists(instance, year, sector):
-    return instance.security_objective_exists(year, sector)
-
-
-@register.simple_tag
-def risk_analysis_exists(instance, year, sector):
-    return instance.risk_analysis_exists(year, sector)
-
-
-@register.simple_tag
-def get_report_recommandations(instance, year, sector):
-    return instance.get_report_recommandations(year, sector)
-
-
-@register.simple_tag
 def settings_value(name):
     return getattr(settings, name, "")
