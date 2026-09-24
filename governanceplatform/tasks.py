004e8f75720b5ef6a7175a1bd38b48270fd3d674
Juan Rocha <juan.rocha@nc3.lu>
2026-09-24 13:17:11 +0200

    [tasks] Removed unnecessary Django initialization code from tasks.py files

--- governanceplatform/tasks.py
+++ governanceplatform/tasks.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python3
 
-import os
-
-import django
-
-# django init
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "governanceplatform.settings")
-django.setup()
-
-from governanceplatform.scripts import (  # noqa: E402 F401
+from governanceplatform.scripts import (  # noqa: F401
     clean_incident_user,
     unactive_account_cleaning,
 )
--- incidents/tasks.py
+++ incidents/tasks.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python3
 
-import os
-
-import django
-
-# django init
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "governanceplatform.settings")
-django.setup()
-
-from incidents.scripts import (  # noqa: E402 F401
+from incidents.scripts import (  # noqa: F401
     email_reminder,
     incident_cleaning,
     log_cleaning,
